4c8917e1dfad5b4f0d3cd23cee3e201ca7cc6f4d
DocShtein <39937490+DocShtein@users.noreply.github.com>
2022-05-16 14:13:12 +0300

    Update fetch_nasa.py

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -1,9 +1,18 @@
+import datetime
 import os
 
 import requests
+from urllib.parse import urlsplit, unquote
 from dotenv import load_dotenv
 
 import api_file_operations
+
+
+def get_image_extension(url):
+    unquoted_link = unquote(url)
+    parsed_url = urlsplit(unquoted_link)
+    name, ext = os.path.splitext(parsed_url.path)
+    return ext
 
 
 def fetch_astronomy_picture_of_the_day(params):
@@ -15,13 +24,24 @@ def fetch_astronomy_picture_of_the_day(params):
         raise requests.exceptions.HTTPError(decoded_response['errors'])
 
     links = []
+
     try:
         for image in decoded_response:
             links.append(image['url'])
     except KeyError:
         pass
 
-    api_file_operations.write_nasa_apod_images(links, params)
+    for image_url in links:
+        image_response = requests.get(image_url, params=params)
+        image_response.raise_for_status()
+        for image_number, image_name in enumerate(image_response):
+            image_name = 'nasa_apod'
+            for extension in links:
+                file_path = f'images/{image_number}_{image_name}{get_image_extension(extension)}'
+
+        api_file_operations.download_images(
+            file_path, image_url, params=params
+        )
 
 
 def fetch_earth_polychromatic_image(params):
@@ -32,7 +52,16 @@ def fetch_earth_polychromatic_image(params):
     if 'errors' in decoded_response:
         raise requests.exceptions.HTTPError(decoded_response['errors'])
 
-    api_file_operations.write_nasa_epic_images(decoded_response, params)
+    for dictionary in decoded_response:
+        for _ in dictionary.items():
+            converted_date = datetime.datetime.fromisoformat(dictionary['date'])
+            formatted_date = converted_date.strftime("%Y/%m/%d")
+            image_url = f'https://api.nasa.gov/EPIC/archive/natural/{formatted_date}/png/{dictionary["image"]}.png'
+            file_path = f'images/{dictionary["image"]}.png'
+
+            api_file_operations.download_images(
+                file_path, image_url, params=params
+            )
 
 
 def main():
