1d06d3cd6b92d52f353c6a708b399a487a284aa7
DocShtein <39937490+DocShtein@users.noreply.github.com>
2022-05-16 14:13:34 +0300

    Update post_images.py

--- post_images.py
+++ post_images.py
@@ -13,7 +13,10 @@ def post_images(amount_of_time_delay, bot_token):
             for file in files:
                 filelist.append(os.path.join(root, file))
         for name in filelist:
-            bot.send_document(chat_id=os.getenv('CHAT_ID'), document=open(name, 'rb'))
+            with open(name, 'rb') as document:
+                bot.send_document(
+                    chat_id=os.getenv('CHAT_ID'), document=document
+                )
             time.sleep(amount_of_time_delay)
 
 
