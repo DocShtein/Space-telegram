c94ea1830ada4a2f0f9aa0fd54b65f9d0bac4f87
DocShtein <39937490+DocShtein@users.noreply.github.com>
2022-05-17 20:47:45 +0300

    Update api_file_operations.py

--- api_file_operations.py
+++ api_file_operations.py
@@ -1,8 +1,8 @@
 import requests
 
 
-def download_images(filepath, image_url, params=None):
-    image_response = requests.get(image_url, params)
-    image_response.raise_for_status()
+def download_image(filepath, url, params=None):
+    response = requests.get(url, params)
+    response.raise_for_status()
     with open(filepath, 'wb') as file:
-        file.write(image_response.content)
+        file.write(response.content)
