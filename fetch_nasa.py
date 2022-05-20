2b579a4cbfdff5e1620541f7e611b12089d67304
DocShtein <39937490+DocShtein@users.noreply.github.com>
2022-05-20 13:50:04 +0300

    Update fetch_nasa.py

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -2,17 +2,9 @@ import datetime
 import os
 
 import requests
-from urllib.parse import urlsplit, unquote
 from dotenv import load_dotenv
 
 import api_file_operations
-
-
-def get_image_extension(url):
-    unquoted_link = unquote(url)
-    parsed_url = urlsplit(unquoted_link)
-    name, ext = os.path.splitext(parsed_url.path)
-    return ext
 
 
 def fetch_astronomy_picture_of_the_day(params):
@@ -34,7 +26,7 @@ def fetch_astronomy_picture_of_the_day(params):
     image_name = 'nasa_apod'
 
     for image_number, image_url in enumerate(links):
-        file_path = f'images/{image_number}_{image_name}{get_image_extension(image_url)}'
+        file_path = f'images/{image_number}_{image_name}{api_file_operations.get_image_extension(image_url)}'
         api_file_operations.download_image(
             file_path, image_url, params=params
         )
@@ -48,11 +40,11 @@ def fetch_earth_polychromatic_image(params):
     if 'errors' in decoded_response:
         raise requests.exceptions.HTTPError(decoded_response['errors'])
 
-    for dictionary in decoded_response:
-        converted_date = datetime.datetime.fromisoformat(dictionary['date'])
+    for photo_metadata in decoded_response:
+        converted_date = datetime.datetime.fromisoformat(photo_metadata['date'])
         formatted_date = converted_date.strftime("%Y/%m/%d")
-        image_url = f'https://api.nasa.gov/EPIC/archive/natural/{formatted_date}/png/{dictionary["image"]}.png'
-        file_path = f'images/{dictionary["image"]}.png'
+        image_url = f'https://api.nasa.gov/EPIC/archive/natural/{formatted_date}/png/{photo_metadata["image"]}.png'
+        file_path = f'images/{photo_metadata["image"]}.png'
 
         api_file_operations.download_image(
             file_path, image_url, params=params
